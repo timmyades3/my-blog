2e604f6b44b3377f17ce07d7b7eb23e1d9efe01a
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-02-16 21:08:59 +0100

    changes

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('MYBLOG_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
