7f04642827f5f2e43658e06df8659b7a74e67356
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-02-20 08:37:08 +0100

    changes in settings.py

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'BLOG.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-      }
-   #'default': {
-   #     'ENGINE': 'django.db.backends.postgresql',
-   #     'NAME': os.environ.get('PGDATABASE'),
-   #      'USER': os.environ.get('PGUSER'),
-   #     'PASSWORD': os.environ.get('PGPASSWORD'),
-    #    'HOST': os.environ.get('PGHOST'),
-   #     'PORT': '5432',
-    #}
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #   'NAME': BASE_DIR / 'db.sqlite3',
+    #  }
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'HOST': os.environ.get('PGHOST'),
+        'PORT': '5432',
+    }
 }
 
 
