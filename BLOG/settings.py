4696dca560e45960d8ab837f7b76eae5a71de04d
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2023-12-24 08:53:29 +0100

    Update settings.py

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
    # }
 }
 
-database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+#database_url = os.environ.get("DATABASE_URL")
+#DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 
