03c47b280bf1a4e07e5dd0b9113b3764041ff6b9
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2023-12-24 08:52:15 +0100

    Update settings.py

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -87,9 +87,17 @@ WSGI_APPLICATION = 'BLOG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-      }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('PGDATABASE'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASSWORD'),
+        'HOST': config('PGHOST'),
+        'PORT': '5432',
+    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #   'NAME': BASE_DIR / 'db.sqlite3',
+    #  }
    #'default': {
    #     'ENGINE': 'django.db.backends.postgresql',
    #     'NAME': os.environ.get('PGDATABASE'),
